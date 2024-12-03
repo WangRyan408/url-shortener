2ef61d2056adf73ef5b624bc14dad839cdb8620f
tigati <packudjj@gmail.com>
2024-12-03 01:45:21 -0800

    add ui for register page

--- client/src/component/navbar.jsx
+++ client/src/component/navbar.jsx
@@ -1,13 +1,12 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import './navbar.css'; // Import the CSS file
 
-
-export default function navbar() {
+export default function Navbar() {
   return (
-    <nav>
-        <Link to='/'>Home</Link>
-        <Link to ='/register'>Register</Link>
-        <Link to ='/login'>Login</Link>
-
+    <nav className="navbar">
+      <Link className="nav-link" to="/">Home</Link>
+      <Link className="nav-link" to="/register">Register</Link>
+      <Link className="nav-link" to="/login">Login</Link>
     </nav>
-  )
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
-//import React from 'react'
-
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      style={{
+        textAlign: "center", // Center the text horizontally
+        marginTop: "40px", // Add some space from the top
+        fontFamily: "'Arial', sans-serif", // Change the font
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "3.3rem", // Large font size
+          color: "#ffffff", // Bright blue color for emphasis
+          marginBottom: "20px",
+        }}
+      >
+        Welcome to Url Shortener
+      </h1>
+
+      <p
+      
+        style={{
+          fontSize: "1.4rem", // Slightly larger font for the message
+          color: "#ffffff", // Subtle text color
+        }}
+      >
+        Explore the coolest features of Url Shortener and enjoy the experience.
+      </p>
+    </div>
+  );
 }
